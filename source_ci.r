10830d2313f0c9d15c022bb09a64db09121f5672
EOneita <eoneitadavis@gmail.com>
2020-03-04 13:42:56 -0800

    adding the package to the repo
    hoping this works 🤞🏼

---
+++ source_ci.r
@@ -0,0 +1,7 @@
+structure(list("ci", structure(list(function (x, confidence = 0.95, 
+    alpha = 1 - confidence, ...) 
+UseMethod("ci"), function (x, confidence = 0.95, alpha = 1 - 
+    confidence, ...) 
+UseMethod("ci")), .Names = c("package:gmodels", "")), c("package:gmodels", 
+"namespace:gmodels"), c(TRUE, FALSE), c(FALSE, TRUE)), .Names = c("name", 
+"objs", "where", "visible", "dups"), class = "getAnywhere")
